31d0d09f4f2475ef6a82bef4b0e47db5279642e1
Maxime Pia <maxime.pia@neuf.fr>
2015-08-29 13:36:21 +0200

    Fix indentation in lua file

--- lua/selectionConverter.lua
+++ lua/selectionConverter.lua
@@ -2,31 +2,31 @@
 
 function updateClipboard(threadObj)
 
-    -- ENCODING among -detect-enc, -sjis, -utf16-be, -utf16-le, -utf8
-    local ENCODING = "-detect-enc"
-    -- 0 <= DEBUG_LEVEL <= 5
-    local DEBUG_LEVEL = 0
-    -- any positive or negative integer
-    local STRICTNESS = 20
+	-- ENCODING among -detect-enc, -sjis, -utf16-be, -utf16-le, -utf8
+	local ENCODING = "-detect-enc"
+	-- 0 <= DEBUG_LEVEL <= 5
+	local DEBUG_LEVEL = 0
+	-- any positive or negative integer
+	local STRICTNESS = 20
 
 
 	local hexView = getMemoryViewForm().HexadecimalView
 	local previousBytes = {}
 	local handle = io.popen(
-        "java.exe -jar \"" .. getCheatEngineDir() ..
-        "autorun\\HexToString.jar\" " ..
-        ENCODING .. " -d=" .. DEBUG_LEVEL .. " -s=" .. STRICTNESS,
-        "w"
-    )
+		"java.exe -jar \"" .. getCheatEngineDir() ..
+		"autorun\\HexToString.jar\" " ..
+		ENCODING .. " -d=" .. DEBUG_LEVEL .. " -s=" .. STRICTNESS,
+		"w"
+	)
 	local selectionSize = 0
 
-    getMainForm().OnClose = function(sender)
-        pcall(function()
-            handle:write("exit")
-            handle:close()
-        end)
-        closeCE()
-    end
+	getMainForm().OnClose = function(sender)
+		pcall(function()
+			handle:write("exit")
+			handle:close()
+		end)
+		closeCE()
+	end
 
 	while true do
 
@@ -36,19 +36,19 @@ function updateClipboard(threadObj)
 			local bytes=readBytes(hexView.SelectionStart, selectionSize+1,true)
 			if bytes ~= nil then
 
-                local s = ""
-                for i = 1, table.getn(bytes) do
-                    s = s .. string.format("%02x", bytes[i])
-                end
-                handle:write(s .. "\n")
-                handle:flush()
-                previousBytes = bytes
+				local s = ""
+				for i = 1, table.getn(bytes) do
+					s = s .. string.format("%02x", bytes[i])
+				end
+				handle:write(s .. "\n")
+				handle:flush()
+				previousBytes = bytes
 
 			end
 
 		end
 
-        sleep(300)
+		sleep(300)
 
 	end
 
