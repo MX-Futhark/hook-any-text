32ae7b2f5c27b66db50422f952931c80a9d37042
Maxime Pia <maxime.pia@neuf.fr>
2015-10-29 06:52:31 +0100

    Add '&' to the possible chars in a JP string

--- src/main/java/hextostring/evaluate/string/JapaneseStringEvaluator.java
+++ src/main/java/hextostring/evaluate/string/JapaneseStringEvaluator.java
@@ -16,7 +16,7 @@ public class JapaneseStringEvaluator extends ReadableStringEvaluator {
 
 	// valid Japanese characters
 	public static final String JAPANESE_CHARS_REGEX =
-		"([a-zA-Z0-9#]|[ａ-ｚＡ-Ｚ０-９]|[\u3000-\u303F]|\u2048|\u2049|" +
+		"([a-zA-Z0-9#&]|[ａ-ｚＡ-Ｚ０-９]|[\u3000-\u303F]|\u2048|\u2049|" +
 		"[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|“|”|…|—|" +
 		"[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B)+";
 
